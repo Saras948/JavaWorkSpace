da49bf49c8102c001bcda165c816e4a9ee2bbd6b
Gaurav Kumar Sharma <saraswat948@gmail.com>
2024-03-16 22:43:43 +0530

    Intial Commit

--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
+import java.util.Date;
+
 public class Main{
     public static void main(String[] args) {
         System.out.println("Hello I am Java Programmer");
         System.out.println("This code is running on jdk 21");
         System.out.println("Thank You For Choosing Java as Programming Language");
+        System.out.println("Current Date : " + new Date());
     }
 }
