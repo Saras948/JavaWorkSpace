706594629e06f0420f9867a62659d6bd188c5671
Gaurav Kumar Sharma-dxc <gaurav-kumar.sharma-dxc@hpe.com>
2024-03-16 22:41:00 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Hello I am Java Programmer");
+        System.out.println("This code is running on jdk 21");
+        System.out.println("Thank You For Choosing Java as Programming Language");
+    }
+}
